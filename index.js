f70bee92ff4fdae9d9e456fbc7eb44b218747bfe
Graham Wood <graham@MacBook-Pro-2.local>
2017-04-25 14:52:15 -0500

    parsed headers for the infomation required by project

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
 const chalk = require('chalk');
+
+// listen on port set in env in deveopment or just port 80 if no env files exist
+const port = process.env.PORT || 80;
 
 const app = express();
 
 // listen to get requests on any url
 app.get('*', (req, res) => {
-  res.send('Hello...');
+
+  let userAgent = req.get('User-Agent');
+  let software = userAgent.substr(0,userAgent.indexOf(')')).slice(userAgent.indexOf('(')+1);
+
+  let acceptLanguage = req.get('Accept-Language');
+  let language = acceptLanguage.substr(0,acceptLanguage.indexOf(','));
+
+  res.send({
+    'ipaddress': req.ip,
+    'language': language,
+    'software': software
+  });
 });
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(chalk.yellow(`Listening on port: ${port}`));
+});
